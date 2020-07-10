1b8e28981e55e6817a675eea70bc261c7b84095a
delfdum <60744187+delfdum@users.noreply.github.com>
2020-07-10 11:18:47 +0200

    partie auteur

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
 <!--Partie de modèle de barre latérale : sidebar.php  -->
-<aside class="sidebar">
-<!--on déclare une zone de widgets --> 
+<aside class="sidebar <?php if(get_theme_mod('widgets-sidebar')) : echo 'hidden'; endif; ?>" id="widgets-sidebar">
+<!--on déclare une zone de widgets -->
   <?php
     if(is_active_sidebar('widgets-sidebar')) :
       dynamic_sidebar('widgets-sidebar');
