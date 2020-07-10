0b75b57bb1895f2f1426380b8a4de2c5120e2647
delfdum <60744187+delfdum@users.noreply.github.com>
2020-07-10 11:20:42 +0200

    enlever style JS

--- header.php
+++ header.php
@@ -13,7 +13,6 @@
   <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital@0;1&family=Oswald:wght@500;600;700&display=swap" rel="stylesheet">
   <script src="https://kit.fontawesome.com/4e5f136f21.js" crossorigin="anonymous"></script>
-  <script src="./assets/js/main.js" type="text/javascript"></script>
 
   <?php wp_head(); ?>
 
@@ -92,4 +91,5 @@
         <input type="submit" name="submit" value="Rechercher">
       </form>
     </section>
+
   </header>
