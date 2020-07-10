c9e88cd9739d9d6c71bce4b0c1664d7c68e282ba
delfdum <60744187+delfdum@users.noreply.github.com>
2020-07-10 11:18:22 +0200

    maj

--- page.php
+++ page.php
@@ -1,35 +1,33 @@
 <?php
 // Modèle de page  : page.php
 // Modèle utilisé par WP pour afficher toute page du site qui n'est pas la page 'Blog', la page d'un article (single.php) ou la page d'accueil forcée (front-page.php).
- ?>
+?>
 
 <?php get_header(); ?>
 
 <?php if(have_posts()) : ?>
   <?php while(have_posts()) : the_post(); ?>
 
-    <?php if(has_post_thumbnail()) : ?>
-
-      <section class="page-header" style="background-image: url(<?php the_post_thumbnail_url(); ?>);">
+  <!-- ajout attribut style à la section "page-header"
+         pour lui ajouter une image de fond correspondant à la vignette de la page. si (if) bien sure, il y a une vignette -->
+  <?php if(has_post_thumbnail()) : ?>
+    <section class="page-header" style="background-image: url(<?php the_post_thumbnail_url(); ?>);">
       <?php else : ?>
-      <section class="page-header">
-      <?php endif; ?>
-
-    <h1 class="page-title"><?php the_title(); ?></h1>
-    </section>
-
-    <main class="container site-content">
-      <article class="entry page">
-        <section class="entry-content">
-          <?php the_content(); ?>
-
-          <?php comments_template(); ?>
-
+        <section class="page-header">
+        <?php endif; ?>
+        <h1 class="page-title"><?php the_title(); ?></h1>
         </section>
-      </article>
+
+      <main class="container site-content">
+        <article class="entry page">
+          <section class="entry-content">
+          <?php the_content(); ?>
+          <?php comments_template(); ?>
+          </section>
+        </article>
 
     <?php endwhile; ?>
   <?php endif; ?>
-  </main>
+    </main>
 
 <?php get_footer(); ?>
