83d8dd87c5068e30ab903a1d724f113bfd8ff64b
delfdum <60744187+delfdum@users.noreply.github.com>
2020-07-12 16:02:48 +0200

    maj pas encore fini

--- searchform.php
+++ searchform.php
@@ -1,5 +1,11 @@
 <!-- l'input recherche -->
-<form id="searchform" method="get" action="<?php echo esc_url( home_url( '/' )); ?>">
+<form id="searchform" method="get" action="<?php
+ if (empty($_GET['?s']) || $_GET['?s'] === '') {
+     echo esc_url(home_url('/404'));
+ } else {
+    echo esc_url(home_url('/'));
+}
+?>">
   <label>
     <input type="text" class="search-field" name="s" placeholder="<?php echo get_theme_mod('search_text'); ?>" value="<?php echo get_search_query(); ?>">
   	</label>
